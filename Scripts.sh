7ccf01887f6e91541d239b0109fee27a697978ca
Hariom Kurmi <hariom_k@bt.iitr.ac.in>
2024-10-14 01:27:07 +0530

    Add files via upload

---
+++ Scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!/bin/bash
+
+#to create files
+zcat datas/shuf.a.bed.gz datas/shuf.b.bed.gz | awk '{print $0>>$1".bed"}'
+
+#snakemake rule
+snakemake --snakefile sort_files.smk fully_sorted/sorted_bed_file_per_sample.bed -j1
