74e0dbedcf1d0565741e514f6aadab5c5d9ce7e2
Hariom Kurmi <hariom_k@bt.iitr.ac.in>
2024-10-14 13:02:44 +0530

    Update Scripts.sh

--- Scripts.sh
+++ Scripts.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 #to create files
-zcat datas/shuf.a.bed.gz datas/shuf.b.bed.gz | awk '{print $0>>$1".bed"}'
+zcat datas/shuf.a.bed.gz datas/shuf.b.bed.gz | awk '{print $0>>$1}'
 
 #snakemake rule
 snakemake --snakefile sort_files.smk fully_sorted/sorted_bed_file_per_sample.bed -j1
